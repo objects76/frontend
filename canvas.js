8217ddc9873c725b92e151fb1cb9d8d6ac825f49
jjkim <objects76@gmail.com>
2020-05-07 14:19:32 +0900

    initial commit

---
+++ canvas.js
@@ -0,0 +1,8 @@
+
+
+var canvas = document.querySelector('#canvas1');
+//console.log(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
