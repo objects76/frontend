2df14590db918bbc0ca0626db82362a7f087fca1
jjkim <objects76@gmail.com>
2020-05-07 14:27:52 +0900

    drawing

--- canvas.js
+++ canvas.js
@@ -6,3 +6,28 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
+
+// drawing 
+c.fillStyle = 'rgba(0,0,255, 0.5)';
+
+
+c.beginPath();
+c.arc(200, 300, 30, 0, Math.PI*2, false);
+c.strokeStyle = 'rgba(255,0,255, 1)';
+c.stroke()
+
+for(let i=0; i<1000; ++i) {
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+
+    c.beginPath();
+    c.arc(x,y, 30, 0, Math.PI*2, false);
+    c.strokeStyle = `rgba(${r},${g},${b}, 1)`;
+    c.stroke()
+}
+
+
